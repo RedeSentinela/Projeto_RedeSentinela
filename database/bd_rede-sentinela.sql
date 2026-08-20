b47d96f9f42991a18dee12445dc5acbc81a0704e
EsterRS7 <133404260+EsterRS7@users.noreply.github.com>
2026-08-20 12:05:51 -0300

    updade no bd

--- database/bd_rede-sentinela.sql
+++ database/bd_rede-sentinela.sql
@@ -1,12 +1,12 @@
 CREATE TABLE tipo_conta (
 	tipo_conta_id SERIAL PRIMARY KEY,
 	nome VARCHAR(100) NOT NULL
-)
+);
 
 CREATE TABLE tipo_organizacao (
 	tipo_organizacao_id SERIAL PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL UNIQUE
-)
+);
 
 CREATE TABLE conta (
 	conta_id SERIAL PRIMARY KEY,
@@ -14,13 +14,13 @@ CREATE TABLE conta (
 	email VARCHAR(255) NOT NULL UNIQUE,
 	senha VARCHAR(255) NOT NULL,
 	criado_em TIMESTAMPTZ NOT NULL DEFAULT now()
-)
+);
 
 CREATE TABLE usuario (
 	user_id SERIAL PRIMARY KEY,
 	conta_id INT NOT NULL UNIQUE REFERENCES conta(conta_id),
 	nome VARCHAR(100) NOT NULL
-)
+);
 
 CREATE TABLE organizacao (
 	organizacao_id SERIAL PRIMARY KEY,
@@ -35,7 +35,7 @@ CREATE TABLE organizacao (
 	longitude DECIMAL(9,6) NOT NULL,
 	desc_servicos TEXT NOT NULL,
 	ativo BOOLEAN NOT NULL DEFAULT true
-)
+);
 
 CREATE TABLE contato_emergencia (
 	contato_id SERIAL PRIMARY KEY,
@@ -45,4 +45,9 @@ CREATE TABLE contato_emergencia (
 	relacao VARCHAR(100),
 	prioridade INT,
 	ativo BOOLEAN NOT NULL DEFAULT true
-)
+);
+
+/*-------- seção INSERTS --------*/
+
+INSERT INTO tipo_conta (nome) VALUES ('Usuário');
+INSERT INTO tipo_conta (nome) VALUES ('Organização');
